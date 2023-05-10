6c0f7b24e811fd307aef70185f753c23104a0758
Alisson Calazans <104709707+AlissonCalazans@users.noreply.github.com>
2023-05-09 21:48:40 -0300

    BusOn 0.1

---
+++ inscricao.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <title>BusOn</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css">
+  <!-- css -->
+  <link rel="stylesheet" href="inscricao.php">
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLT">
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <header>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-black">
+      <a class="navbar-brand" href="#">
+        <img src="img/buson-semfundo.png" width="30" height="30" class="d-inline-block align-top" alt="Logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Serviços</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="#">Contatos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled text-white" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline input-group">
+          <input class="form-control mr-sm-2 m-auto col-11 p-2 border-left-5" type="search" placeholder="Pesquisar" aria-label="Search">
+          <div class="input-group-btn ">
+            <button class="btn btn-outline-warning btn-default my-2 my-sm-1 m-3" type="submit">Pesquisar</button>
+          </div>
+        </form>
+      </div>
+    </nav>
+  </header>
+
+  <main>
+
+    <!-- Carrossel-->
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="img/slide1.jpg" width="100" height="450" class="d-block w-100 h-20" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/slide2.jpg" width="100" height="450" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="..." width="100" height="450" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+
+    <div class="container-img">
+      <img src="img/buson-semfundo.png" alt="Logo da BusOn" class="logo">
+      <p>Descubra o mundo conosco!</p>
+    </div>
+
+    <form class="form-horizontal" action="" method="post">
+      <!-- Form inputs -->
+    </form>
+    <!-- Cards-->
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="img/buson-semfundo.png" alt="Imagem de capa do card">
+      <div class="card-body">
+        <h5 class="card-title">Praia Do Mangue</h5>
+        <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+        <a href="#" class="btn btn-primary">Visitar</a>
+      </div>
+    </div>
+  </main>
+
+  <footer>
+    <!-- footer -->
+    <footer>
+      <div class="social-media">
+        <a href="#" class="social-link"><i class="fab fa-facebook-f"></i></a>
+        <a href="#" class="social-link"><i class="fab fa-twitter"></i></a>
+        <a href="#" class="social-link"><i class="fab fa-instagram"></i></a>
+        <a href="#" class="social-link"><i class="fab fa-linkedin-in"></i></a>
+      </div>
+      <p>&copy; 2023 Todos os direitos reservados.</p>
+    </footer>
+  </footer>
+
+  <!-- JavaScript dependencies -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
